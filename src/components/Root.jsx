fef69877bb6a15c5a623cc1a4a0d22eedcfe309a
ahmisrafil <ahmisrafil@yahoo.com>
2023-10-21 03:15:30 +0600

    registretion page added

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,4 +1,5 @@
 
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 
@@ -6,6 +7,7 @@ const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,30 @@ import {
 import './index.css'
 import Root from './components/Root.jsx';
 import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    errorElement: <ErrorPage></ErrorPage>
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element : <Home></Home>,
+      },
+      {
+        path: "/login",
+        element : <Login></Login>
+      },
+      {
+        path:"/register",
+        element : <Register></Register>
+      }
+    ]
   },
 ]);
 
